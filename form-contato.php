74b80f4d50e4fde77f46002f6fdda10ab6ff7f00
slarisz <larissakd@hotmail.com>
2020-11-30 13:22:59 -0300

    fomulario de contato

---
+++ form-contato.php
@@ -0,0 +1,25 @@
+<?php
+
+    $destinatario = "larissakd@hotmail.com";
+
+    $nome = $_REQUEST['nome'];
+    $email = $_REQUEST['email'];
+    $phone = $_REQUEST['phone'];
+    $mensagem = $_REQUEST['mensagem'];
+
+
+    $body = "===================================" . "\n";
+    $body = $body . "FALE CONOSCO - TESTE" . "\n";
+    $body = $body . "===================================" . "\n\n";
+    $body = $body . "Nome Completo: " . $nome . "\n";
+    $body = $body . "Email: " . $email . "\n";
+    $body = $body . "Telefone: " . $email . "\n";
+    $body = $body . "Mensagem: " . $mensagem . "\n\n";
+    $body = $body . "===================================" . "\n";
+
+    mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+
+    header("location:index.html");
+
+?>
